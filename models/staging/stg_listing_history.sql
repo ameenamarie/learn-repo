8f89fe67a45da5fcae9227c37a8b480c5cfb2f40
Ameena Khan <akhan@grove.co>
2019-06-11 10:05:57 -0700

    create stg_listing_history file

---
+++ models/staging/stg_listing_history.sql
@@ -0,0 +1,15 @@
+with source as (
+  select * from source_data.listing_history
+),
+
+renamed as (
+  select
+  listing_id,
+  price,
+  available,
+  "date"
+
+  from source
+)
+
+select * from renamed
