9b38ec30321b604728701ae5f2ef70c0ab42992e
Ameena Khan <akhan@grove.co>
2019-06-11 10:58:30 -0700

    create marts folder

---
+++ models/marts/dim_reviewer.sql
@@ -0,0 +1,39 @@
+{{ config (
+  materialized = 'table'
+  dist = 'review_id'
+  sort = 'review_id'
+) }}
+
+with review as (
+  select 
+    reviewer_name,
+    first_value("date") 
+      over (partition by reviewer_name order by "date" rows 
+        between unbounded preceding and current row) as first_review_at,
+    listings_id,
+    count(review)
+      over (partition by reviewer_name order by "date"
+        rows between unbounded preceding and unbounded following) as total_reviews
+  from stg_reviews
+),
+
+listings as (
+  select
+    listings_id,
+    neighbourhood
+  from stg_listings
+),
+
+joined as (
+  select
+    reviewer_name,
+    first_review_at,
+    listings.neighbourhood,
+    total_reviews
+  from review
+  join listings 
+    on review.listings_id = listings.listings_id
+)
+
+select * from joined
+
